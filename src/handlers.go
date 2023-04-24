2fd650a522d7e15072a9e5af08c5a74dc0343fa7
Chad Edwards <chad@saw.dev>
2023-04-23 22:21:01 -0400

    more options for multi-sumerization

--- src/handlers.go
+++ src/handlers.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
+	"strings"
+	// "github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// 	CheckOrigin: func(r *http.Request) bool {
+// 		return true
+// 	},
+// }
 
 func getStats(w http.ResponseWriter, r *http.Request) {
 	stats := fetchPlayerStats()
@@ -34,49 +33,54 @@ func getAllStatTypesHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getSummarizedStatsHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the statType parameter from the request
-	statType := r.URL.Query().Get("statType")
-	if statType == "" {
+	// Parse the statTypes parameter from the request
+	statTypesParam := r.URL.Query().Get("statType")
+	if statTypesParam == "" {
 		http.Error(w, "statType is required", http.StatusBadRequest)
 		return
 	}
 
-	// Get the aggregated stats and individual stats for the specified statType
-	summarizedStats, individualStats, err := getSummarizedStats(statType)
+	// Split the statTypes parameter into a slice of stat types
+	statTypes := strings.Split(statTypesParam, ",")
+
+	// Get the summarized stats for each stat type and store them in a map
+	aggregatedStats, individualStats, err := getSummarizedStats(statTypes)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Send the aggregated stats and individual stats as a JSON response
+	// Prepare the response JSON
 	response := map[string]interface{}{
-		"summarizedStats": summarizedStats,
+		"aggregatedStats": aggregatedStats,
 		"individualStats": individualStats,
 	}
+
+	// Send the aggregated stats as a JSON response
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer ws.Close()
+// func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+// 	ws, err := upgrader.Upgrade(w, r, nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 		return
+// 	}
+// 	defer ws.Close()
 
-	for {
-		_, _, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
+// 	for {
+// 		_, _, err := ws.ReadMessage()
+// 		if err != nil {
+// 			log.Println("read:", err)
+// 			break
+// 		}
 
-		stats := fetchPlayerStats()
-		err = ws.WriteJSON(stats)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
-}
+// 		stats := fetchPlayerStats()
+// 		err = ws.WriteJSON(stats)
+// 		if err != nil {
+// 			log.Println("write:", err)
+// 			break
+// 		}
+// 	}
+// }
--- src/summarizedstats.go
+++ src/summarizedstats.go
@@ -6,48 +6,53 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-func getSummarizedStats(statType string) (map[string]int64, map[string]map[string]int64, error) {
+func getSummarizedStats(statTypes []string) (map[string]map[string]int64, map[string]map[string]int64, error) {
 	playerNames, err := getAllPlayersFromRedis()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	summarizedStats := make(map[string]int64)
+	aggregatedStats := make(map[string]map[string]int64)
 	individualStats := make(map[string]map[string]int64)
 
 	for _, playerName := range playerNames {
-		pattern := fmt.Sprintf("player_stats:%s:*%s*", playerName, statType)
+		for _, statType := range statTypes {
+			pattern := fmt.Sprintf("player_stats:%s:*%s*", playerName, statType)
 
-		var cursor uint64
-		for {
-			var keys []string
-			keys, cursor, err = rdb.Scan(ctx, cursor, pattern, 10).Result()
-			if err != nil {
-				return nil, nil, err
-			}
-
-			for _, key := range keys {
-				stat, err := rdb.Get(ctx, key).Int64()
-
-				if err == redis.Nil {
-					stat = 0
-				} else if err != nil {
+			var cursor uint64
+			for {
+				var keys []string
+				keys, cursor, err = rdb.Scan(ctx, cursor, pattern, 10).Result()
+				if err != nil {
 					return nil, nil, err
 				}
 
-				summarizedStats[playerName] += stat
+				for _, key := range keys {
+					stat, err := rdb.Get(ctx, key).Int64()
 
-				if individualStats[playerName] == nil {
-					individualStats[playerName] = make(map[string]int64)
+					if err == redis.Nil {
+						stat = 0
+					} else if err != nil {
+						return nil, nil, err
+					}
+
+					if individualStats[playerName] == nil {
+						individualStats[playerName] = make(map[string]int64)
+					}
+					individualStats[playerName][key] = stat
+
+					if aggregatedStats[playerName] == nil {
+						aggregatedStats[playerName] = make(map[string]int64)
+					}
+					aggregatedStats[playerName][statType] += stat
 				}
-				individualStats[playerName][key] = stat
-			}
 
-			if cursor == 0 {
-				break
+				if cursor == 0 {
+					break
+				}
 			}
 		}
 	}
 
-	return summarizedStats, individualStats, nil
+	return aggregatedStats, individualStats, nil
 }
